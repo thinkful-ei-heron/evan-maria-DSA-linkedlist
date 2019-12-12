2f6d71aa6c48a04ad7c60bd764d7c15c2909f776
Evan Vogts <evanjvogts@gmail.com>
2019-12-12 10:48:38 -0600

    #1

--- index.js
+++ index.js
@@ -1,33 +1,82 @@
 //1. Create a linked list class
 
 class _Node {
-    constructor(value, next) {
-        this.value = value;
-        this.next = next;
-    }
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
 }
-
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(item) {
+    this.head = new _Node(item, this.head);
+  }
+  insertLast(item) {
+    if (this.head === null) {
+      this.insertFirst(item);
     }
-    insertFirst(item){
-        this.head = new _Node(item, this.head)
+    else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
     }
-    insertLast(item){
-        let tempNode = this.head
-        while(tempNode.next !== null){
-            tempNode = tempNode.next
-        }
-        tempNode.ext =  new _Node(item, null)
+  }
+  find(item) { 
+    // Start at the head
+    let currNode = this.head;
+    // If the list is empty
+    if (!this.head) {
+      return null;
     }
+    // Check for the item 
+    while (currNode.value !== item) {
+      /* Return null if it's the end of the list 
+               and the item is not on the list */
+      if (currNode.next === null) {
+        return null;
+      }
+      else {
+        // Otherwise, keep looking 
+        currNode = currNode.next;
+      }
+    }
+    // Found it
+    return currNode;
+  }
+  remove(item){ 
+    // If the list is empty
+    if (!this.head) {
+      return null;
+    }
+    // If the node to be removed is head, make the next node head
+    if (this.head.value === item) {
+      this.head = this.head.next;
+      return;
+    }
+    // Start at the head
+    let currNode = this.head;
+    // Keep track of previous
+    let previousNode = this.head;
+
+    while ((currNode !== null) && (currNode.value !== item)) {
+      // Save the previous node 
+      previousNode = currNode;
+      currNode = currNode.next;
+    }
+    if (currNode === null) {
+      console.log('Item not found');
+      return;
+    }
+    previousNode.next = currNode.next;
+  }
 }
 
+let ll = new LinkedList;
 
-let ll = new LinkedList()
 ll.insertFirst(5);
-ll.insertFirst(2); 
-ll.insertLast(10);
 
-
-console.log(LinkedList)
+console.log(ll);
