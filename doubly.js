dc8f1b516294b78141f33fa9ff36fedb16496052
Maria Danielson <mal3905@gmail.com>
2019-12-12 16:59:33 -0500

    added to doubly.js

--- doubly.js
+++ doubly.js
@@ -1,7 +1,8 @@
 class _Node {
-    constructor(value, next) {
-        this.head = null 
-        this.previous = null
+    constructor(value, next, previous) {
+        this.value = value
+        this.previous = previous
+        this.next = next
     }
 }
 
@@ -11,18 +12,58 @@ class DoublyLinkedList {
     }
 
     insertFirst(item) {
-        const newNode = new _Node(item, this.head);
+        const newNode = new _Node(item, this.head, null);
             if( this.head === null) {
                 this.head = newNode
             } else 
             this.head.previous = newNode
             newNode.previous = newNode 
+    }
+
+    insertLast(item) {
+        if (this.head === null) {
+          this.insertFirst(item);
+        }
+        else {
+          let tempNode = this.head;
+          while (tempNode.next !== null) {
+            tempNode = tempNode.next;
+          }
+          tempNode.next = new _Node(item, null);
+        }
       }
+
+      insertBefore(item, before) {
+        let currNode = this.find(before)
+        let tempNode = new _Node (item, currNode, currNode.previous)
+        currNode.previous.next = tempNode;
+        currNode.previous = tempNode
+      }
+
+      
+
+
+
+
+
+    display() {
+        let currNode = this.head;
+        let str = '';
+        while(currNode.next !== null) {
+        str = str + currNode.value + ', ';
+        currNode = currNode.next;
+        }
+        str = str + currNode.value;
+        console.log(str);
+    }
 }
-
-
 
 function main() {
     let DLL = new DoublyLinkedList();
-        DLL.insertFirst('')
+        DLL.insertFirst('Tauron')
+        DLL.insertLast('Picon')
+        DLL.insertBefore('Apple', 'cereal')
+        DLL.display();
 }
+
+main();
