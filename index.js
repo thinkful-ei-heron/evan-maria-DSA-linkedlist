7620e4df3fcb9a0bfa685397dd6e046c66bbd742
Evan Vogts <evanjvogts@gmail.com>
2019-12-12 13:19:54 -0600

    finished #2

--- index.js
+++ index.js
@@ -25,6 +25,29 @@ class LinkedList {
       tempNode.next = new _Node(item, null);
     }
   }
+  insertBefore(item, before) {
+    let currNode = this.head;
+    let tempNode = this.head;
+    while (currNode.value !== before) {
+      tempNode = currNode;
+      currNode = currNode.next;
+    }
+    tempNode.next = new _Node(item, currNode);
+  }
+  insertAfter(item, after) {
+    let currNode = this.find(after);
+    let nextNode = currNode.next;
+    currNode.next = new _Node(item, nextNode);
+  }
+  insertAt(item, index) {
+    let currIndex = 0;
+    let currNode = this.head;
+    while (currIndex !== (index - 1)) {
+      currNode = currNode.next;
+      currIndex++;
+    }
+    currNode.next = new _Node(item, currNode.next.next);
+  }
   find(item) { 
     // Start at the head
     let currNode = this.head;
@@ -73,10 +96,53 @@ class LinkedList {
     }
     previousNode.next = currNode.next;
   }
+  display() {
+    let currNode = this.head;
+    let str = '';
+    while(currNode.next !== null) {
+      str = str + currNode.value + ', ';
+      currNode = currNode.next;
+    }
+    str = str + currNode.value;
+    console.log(str);
+  }
+  size() {
+    let size = 0;
+    let currNode = this.head;
+    while (currNode !== null) {
+      size++;
+      currNode = currNode.next;
+    }
+    return size;
+  }
+  isEmpty(){
+    if(this.head === null){
+      return true;
+    }
+    return false;
+  }
 }
 
 let ll = new LinkedList;
 
 ll.insertFirst(5);
 
-console.log(ll);
+// console.log(ll);
+
+function main() {
+  let SLL = new LinkedList;
+  SLL.insertLast('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+  SLL.insertLast('Tauhida');
+  SLL.remove('squirrel');
+  SLL.insertBefore('Athena', 'Boomer');
+  SLL.insertAfter('Hotdog', 'Helo');
+  SLL.insertAt('Kat', 2);
+  SLL.remove('Tauhida');
+  SLL.display();
+}
+
+main();
